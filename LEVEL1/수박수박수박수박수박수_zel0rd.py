d08a8a666823eee09cea75349822f1523a839c21
zel0rd <zelord.kwoun@gmail.com>
2018-11-13 22:52:36 +0900

    수박수정

--- LEVEL1/수박수박수박수박수박수_zel0rd.py
+++ LEVEL1/수박수박수박수박수박수_zel0rd.py
@@ -1,6 +1,6 @@
 def solution(n):
     if(n % 2 == 1):
-        answer = int(n / 2) *"수박" + "수"
+        answer = int(n / 2) * "수박" + "수"
     else:
         answer = int(n / 2) * "수박"
     return answer
